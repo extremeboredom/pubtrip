0d02e761f71e8e049579da9e9395a549a1f4b6fa
Adam Cox <adam@adamcox.me>
2012-08-22 20:24:18 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pubtrip::Application.config.secret_token = '3ab96e7049e833492e034ba053ba693906ba4c23f2ba8e3d788f3b8218a55d6d628d59b6175ed2afaea95f8c3e17ce35d089f30b78e3e4c45a452bf4570852a6'
