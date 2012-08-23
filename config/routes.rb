4cb86dc95e43f9cb68420fd7bbd5761577392e7b
Adam Cox <adam@adamcox.me>
2012-08-23 20:07:07 +0100

    Added index page and removed unwanted stylesheets.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Pubtrip::Application.routes.draw do
+  get "home/index"
+
   resources :pubs
 
   resources :trips
@@ -52,7 +54,7 @@ Pubtrip::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
