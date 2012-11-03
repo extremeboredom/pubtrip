c922cf27641a204f5e12b8f9cfd465bf50bb7aed
Adam Cox <adam@adamcox.me>
2012-11-03 12:32:48 +0000

    Added Remember Me functionality to the database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121103113114) do
+ActiveRecord::Schema.define(:version => 20121103123026) do
 
   create_table "menu_groups", :force => true do |t|
     t.string   "name"
@@ -45,11 +45,15 @@ ActiveRecord::Schema.define(:version => 20121103113114) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",            :null => false
+    t.string   "email",                        :null => false
     t.string   "crypted_password"
     t.string   "salt"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
   end
+
+  add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
 
 end
