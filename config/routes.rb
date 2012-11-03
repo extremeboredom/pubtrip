3534fb42867bfef3d0ceb2753c09bb4e1dfc623b
Adam Cox <adam@adamcox.me>
2012-11-03 12:33:18 +0000

    Added login and logout links to the site.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Pubtrip::Application.routes.draw do
 
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
+  get "signup" => "users#new", :as => "signup"
   resources :users
   resources :sessions
+  
   resources :pubs
   match 'pubs/:id/menu' => 'menu#show', :as => :menu
 
