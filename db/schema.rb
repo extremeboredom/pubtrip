3e94966a5d6c108149380e1ab8ee70caa17fb248
Adam Cox <adam@adamcox.me>
2012-11-03 11:39:13 +0000

    Added sorcery with users model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120825143042) do
+ActiveRecord::Schema.define(:version => 20121103113114) do
 
   create_table "menu_groups", :force => true do |t|
     t.string   "name"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(:version => 20120825143042) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "pub_id"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",            :null => false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
 
 end
