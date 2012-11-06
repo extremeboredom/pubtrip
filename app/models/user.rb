bc8c76d61bc0cd5babc829323b1b10afef79f136
Adam Cox <adam@adamcox.me>
2012-11-06 16:52:59 +0000

    Added helpers to determine how users relate to trips.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@ class User < ActiveRecord::Base
   validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
+
+  def organising?(trip)
+    trip.organiser == self
+  end
+
+  def attending?(trip)
+    self.trips.exists? trip
+  end
 end
