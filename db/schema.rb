c554778434ddb0dbc2f15f354c84ba8d6ef32b7a
Adam Cox <adam@adamcox.me>
2012-11-06 17:36:22 +0000

    Added first and last name to the database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121106132859) do
+ActiveRecord::Schema.define(:version => 20121106173536) do
 
   create_table "attendees", :force => true do |t|
     t.integer  "user_id"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(:version => 20121106132859) do
     t.datetime "updated_at",                   :null => false
     t.string   "remember_me_token"
     t.datetime "remember_me_token_expires_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
