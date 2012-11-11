136dd9527387fc5f06e8e95b2597a86d8b90725d
Adam Cox <adam@adamcox.me>
2012-11-11 13:20:49 +0000

    Added cutoff date to the Trip model.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,13 +1,14 @@
 class Trip < ActiveRecord::Base
-  attr_accessible :date, :name, :pub_id, :date_date, :date_time
+  attr_accessible :date, :name, :pub_id, :date_date, :date_time, :order_cutoff
   attr_accessor :date_date, :date_time
+  attr_accessor :order_cutoff_date, :order_cutoff_time
 
   belongs_to :pub
   belongs_to :organiser, :class_name => "User", :foreign_key => "user_id"
   has_many :attendees
   has_many :users, :through => :attendees
 
-  before_validation :make_date
+  before_validation :make_date, :make_order_cutoff
   validates :name, :presence => true
   validates :date, :presence => true
   validates :pub, :presence => true
@@ -44,6 +45,38 @@ class Trip < ActiveRecord::Base
 
   def date_time=(new_time)
     @date_time = self.string_to_datetime(new_time, "%H:%M")
+  end
+
+  def make_order_cutoff
+    if @order_cutoff_date.present? && @order_cutoff_time.present?
+      self.order_cutoff = DateTime.new(@order_cutoff_date.year,
+                                       @order_cutoff_date.month,
+                                       @order_cutoff_date.day,
+                                       @order_cutoff_time.hour,
+                                       @order_cutoff_time.min)
+    end
+  end
+
+  def order_cutoff_date
+    return @order_cutoff_date if @order_cutoff_date.present?
+    return self.order_cutoff if self.order_cutoff.present?
+    return nil
+  end
+
+  def order_cutoff_time
+    return @order_cutoff_time if @order_cutoff_time.present?
+    return self.order_cutoff if self.order_cutoff.present?
+    return nil
+  end 
+
+
+  def order_cutoff_date=(new_date)
+    @order_cutoff_date = self.string_to_datetime(new_date, "%d/%m/%Y")
+  end
+
+
+  def order_cutoff_time=(new_time)
+    @order_cutoff_time = self.string_to_datetime(new_time, "%H:%M")
   end
 
   protected
