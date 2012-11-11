4d35d93e3b0dce8ff86b82296edeef4758e5a2b9
Adam Cox <adam@adamcox.me>
2012-11-11 15:51:46 +0000

    Include orders by in the trip title

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -2,4 +2,14 @@ module TripsHelper
   def number_of_orders(trip)
     trip.attendees.count(:joins => :order)
   end
+
+  def trip_title(trip)
+    title = "#{trip.date.strftime '%d/%m/%Y %H:%M'} at #{trip.pub.name}"
+    if trip.order_cutoff
+      day_format_string = ''
+      day_format_string = '%d/%m/%Y ' unless trip.date.to_date === trip.order_cutoff.to_date
+
+      title << " (orders by #{trip.order_cutoff.strftime(day_format_string + '%H:%M')})"
+    end
+  end
 end
