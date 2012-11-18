394696de63fe480d06a17d4da8760c39e024a460
Adam Cox <adam@adamcox.me>
2012-11-18 21:05:35 +0000

    Fixed attendees controller test

--- test/functional/attendees_controller_test.rb
+++ test/functional/attendees_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class AttendeesControllerTest < ActionController::TestCase
+  include Sorcery::TestHelpers::Rails
+
   test "should get create" do
-    get :create
-    assert_response :success
+    @user = users(:test_user)
+    login_user
+    get :create, {trip_id: trips(:first_trip)}
+    assert_redirected_to trip_path(trips(:first_trip))
   end
 
 end
