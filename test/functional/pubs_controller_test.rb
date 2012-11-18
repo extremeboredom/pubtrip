c5d9aa49d5227ee00523a113944973ead9253a40
Adam Cox <adam@adamcox.me>
2012-11-18 20:57:32 +0000

    Fixed Trips controller tests.

--- test/functional/pubs_controller_test.rb
+++ test/functional/pubs_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class PubsControllerTest < ActionController::TestCase
+  include Sorcery::TestHelpers::Rails
+
   setup do
+    @user = users(:test_user)
     @pub = pubs(:generous_briton)
   end
 
@@ -12,11 +15,13 @@ class PubsControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    login_user
     get :new
     assert_response :success
   end
 
   test "should create pub" do
+    login_user
     assert_difference('Pub.count') do
       post :create, pub: { name: @pub.name }
     end
@@ -30,16 +35,19 @@ class PubsControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    login_user
     get :edit, id: @pub
     assert_response :success
   end
 
   test "should update pub" do
+    login_user
     put :update, id: @pub, pub: { name: @pub.name }
     assert_redirected_to pub_path(assigns(:pub))
   end
 
   test "should destroy pub" do
+    login_user
     assert_difference('Pub.count', -1) do
       delete :destroy, id: @pub
     end
