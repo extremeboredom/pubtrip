528c343eb1d619147c6b115e4f858399b5b51401
Adam Cox <adam@adamcox.me>
2012-12-04 20:25:33 +0000

    Added additional Group unit tests.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -10,4 +10,18 @@ class UserTest < ActiveSupport::TestCase
     assert group.invalid?
     assert_equal ["can't be blank"], group.errors[:name]
   end
+
+  test "owner is required" do
+    group = Group.new
+    assert group.invalid?
+    assert_equal ["can't be blank"], group.errors[:owner]
+  end
+
+  test "with name and owner is valid" do
+    group = Group.new
+    group.name = 'My First Group'
+    group.owner = users(:test_user)
+    assert group.valid?
+  end
+
 end
