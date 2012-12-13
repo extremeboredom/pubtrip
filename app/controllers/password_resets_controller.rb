4e7b8380700d59069a2c26ef165ebf835a6c2a78
Adam Cox <adam@adamcox.me>
2012-12-13 19:46:33 +0000

    Can now post to Password Reset Create.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,13 @@
 class PasswordResetsController < ApplicationController
   def create
+    @user = User.find_by_email(params[:email])
+        
+    # This line sends an email to the user with instructions on how to reset their password (a url with a random token)
+    @user.deliver_reset_password_instructions! if @user
+        
+    # Tell the user instructions have been sent whether or not email was found.
+    # This is to not leak information to attackers about which emails exist in the system.
+    redirect_to(root_path, :notice => 'Instructions have been sent to your email.')
   end
 
   def edit
--- test/functional/password_resets_controller_test.rb
+++ test/functional/password_resets_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class PasswordResetsControllerTest < ActionController::TestCase
+  include ActionMailer::TestHelper
+
   test "should get edit with valid token" do 
     forgetful_user = users(:anthony)
 
@@ -17,5 +19,27 @@ class PasswordResetsControllerTest < ActionController::TestCase
     assert_equal nil, assigns(:user)
   end
 
+  test "should send mail when created a password reset" do
+    unforgetful_user = users(:spencer)
+
+    assert_emails 1 do
+      post :create, email: unforgetful_user.email
+    end
+
+    assert_equal unforgetful_user, assigns(:user)
+    assert_not_nil assigns(:user).reset_password_token
+
+    assert_redirected_to root_path
+  end
+
+  test "should not send mail when created a password reset using invalid email" do
+    assert_no_emails do
+      post :create, email: 'definitelynotanemail@example.com'
+    end
+
+    assert_nil assigns(:user)
+
+    assert_redirected_to root_path
+  end
 
 end
