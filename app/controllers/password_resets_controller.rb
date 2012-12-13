4284bccef25d662b9de5a396d1e9a530dda09495
Adam Cox <adam@adamcox.me>
2012-12-13 19:27:35 +0000

    Can now get the edit page.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,6 +3,9 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(@token)
+    not_authenticated unless @user
   end
 
   def update
--- test/functional/password_resets_controller_test.rb
+++ test/functional/password_resets_controller_test.rb
@@ -1,19 +1,21 @@
 require 'test_helper'
 
 class PasswordResetsControllerTest < ActionController::TestCase
-  test "should get create" do
-    get :create
+  test "should get edit with valid token" do 
+    forgetful_user = users(:anthony)
+
+    get :edit, id: forgetful_user.reset_password_token
     assert_response :success
+
+    assert_equal forgetful_user, assigns(:user)
   end
 
-  test "should get edit" do
-    get :edit
-    assert_response :success
+  test "should not get edit with invalid token" do 
+    get :edit, id: 'invalid_token'
+    assert_redirected_to login_path
+
+    assert_equal nil, assigns(:user)
   end
 
-  test "should get update" do
-    get :update
-    assert_response :success
-  end
 
 end
