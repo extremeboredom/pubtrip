a748a494afb149b499f1f6562b233532ea094009
Adam Cox <adam@adamcox.me>
2013-01-07 22:25:13 +0000

    Added a test to check that orders cannot be placed after cutoff.

--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class OrdersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Sorcery::TestHelpers::Rails
+
+  test "placing an order after the cutoff time should fail" do
+    @user = users(:bob)
+    login_user
+
+    trip = trips(:expiring_trip)
+
+    assert_no_difference('Order.count') do
+      post :create, {
+          order: {
+              notes: 'Sausage, egg and Chips'
+          },
+          trip_id: trip
+      }
+    end
+  end
+
 end
