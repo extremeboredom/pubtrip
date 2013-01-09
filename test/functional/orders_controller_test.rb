e7be42c07695ebf28146b0d4920af12b8ac0bfe8
Adam Cox <adam@adamcox.me>
2013-01-09 13:03:42 +0000

    Added tests that check that orders are creatable.

--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -40,4 +40,64 @@ class OrdersControllerTest < ActionController::TestCase
     assert_redirected_to trip_path trip
   end
 
+  test "placing an order before the cutoff time should succeed" do
+    @user = users(:bob)
+    login_user
+
+    notes = 'Sausage, egg and Chips'
+    trip = trips(:unexpired_trip)
+    attendee = attendees(:unexpired_trip_attendee)
+
+    assert_difference('Order.count') do
+      post :create, {
+          order: {
+              notes: notes
+          },
+          trip_id: trip
+      }
+    end
+
+    created_order = assigns(:order)
+    assert_not_nil created_order
+    assert_equal attendee, created_order.attendee
+    assert_equal notes, created_order.notes
+
+    assert_redirected_to trip_order_path trip, created_order
+  end
+
+  test "placing an order while logged out should fail" do
+    notes = 'Sausage, egg and Chips'
+    trip = trips(:unexpired_trip)
+
+    assert_no_difference('Order.count') do
+      post :create, {
+          order: {
+              notes: notes
+          },
+          trip_id: trip
+      }
+    end
+
+    assert_redirected_to login_path
+  end
+
+  #test "placing an order when you are not attending the trip should fail" do
+  #  @user = users(:spencer)
+  #  login_user
+  #
+  #  notes = 'Sausage, egg and Chips'
+  #  trip = trips(:unexpired_trip)
+  #
+  #  assert_no_difference('Order.count') do
+  #    post :create, {
+  #        order: {
+  #            notes: notes
+  #        },
+  #        trip_id: trip
+  #    }
+  #  end
+  #
+  #  assert_redirected_to trip_path trip
+  #end
+
 end
